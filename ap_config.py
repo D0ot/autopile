3a926b5cd785c13d02ece2b104d00d31d158ed9d
D0ot <1306793135@qq.com>
2019-06-08 03:47:17 +0800

    amend for last commit

--- ap_config.py
+++ ap_config.py
@@ -1,11 +1,65 @@
-#!python3
+#!/usr/bin/env python3
 ###########################
 #  This script use pipenv #
 ###########################
 
-# This is a config file for autopile.py
+# This is a config file for Autopile
+# global 
+log = False
+logFile = ''
+maxTryTimes = 10
 
+
+# configure for main scripts 
+globalCwd = '/home/doot/robocup3d/autopile'
 repository = 'git@github.com:D0ot/Learnse.git'
 projectname = 'Learnse'
+# make -j(thread)
+thread = 14
+
+
+# configure for ap_email.py
+# when an error occours receivers will get nofified.
+smtpServer = 'smtp.office365.com'
+smtpServerPort = 587
+sender = 'robocup3dnotify@outlook.com'
+receivers = ['1306793135@qq.com']
+username = 'robocup3dnotify@outlook.com'
+
+# it is not real password, it is just a permission code
+password = 'robocup3d' 
+
+# add send self to receiver
+# or it will get blocked smtp.163.com
+if smtpServer.find('@163.com') != -1 :
+    receivers += [sender]
+
+
+def Context(context):
+    if context == 'test':
+        log = False
+        logFile = ''
+        maxTryTimes = 10
+        globalCwd = '/home/doot/robocup3d/autopile'
+        repository = 'git@github.com:D0ot/Learnse.git'
+        projectname = 'Learnse'
+        thread = 14
+        smtpServer = 'smtp.office365.com'
+        smtpServerPort = 587
+        sender = 'robocup3dnotify@outlook.com'
+        receivers = ['1306793135@qq.com']
+        username = 'robocup3dnotify@outlook.com'
+        password = 'robocup3d' 
+        if smtpServer.find('@163.com') != -1 :
+            receivers += [sender]
+
+    if context == 'log':
+        log = True
+        logFile = './log.txt'
+
+
+if __name__ == '__main__':
+    print("ap_config.py is just configure file, exit...")
+    exit(0)
 
 
--- autopile.py
+++ autopile.py
@@ -1,4 +1,4 @@
-#!python3
+#!/usr/bin/env python3
 
 ##########################
 # this script use pipenv #
@@ -7,15 +7,63 @@
 import ap_config as cfg
 import sh
 import io
+import ap_email
 if __name__ == '__main__':
     print('Autocompile.py is running...')
-    try:
-        stdoutput = io.StringIO()
-        sh.ls(_out=stdoutput)
-        print(stdoutput.getvalue())
-        sh.git.clone(cfg.repository)
-        sh.cd(cfg.projectname)
+    tryTimes = 1
+    while tryTimes <= cfg.maxTryTimes:
+        try:
+            sh.cd(cfg.globalCwd)
+
+            stdoutput = io.StringIO()
+            sh.ls('-d',sh.glob('*/') ,_out=stdoutput)
+            dirs = [ _[:-1] for _ in stdoutput.getvalue().split()]
+            nameIsIn = False
+            for x in dirs:
+                if x == cfg.projectname:
+                    nameIsIn = True
+                    break
         
-    except Exception as e:
-        print('Exception', e)
-        exit(1)
+            if nameIsIn == True:
+                print("Remove old ", cfg.projectname)
+                sh.rm('-r', cfg.projectname)
+
+            sh.git.clone(cfg.repository)
+            sh.cd(cfg.projectname)
+            sh.cmake('.')
+            sh.make('-j{}'.format(cfg.thread))
+            break
+        except Exception as e:
+            print("Build Failed")
+            print("Try : ", tryTimes)
+            print('Exception Caught:', e)
+            print("Sending Email to Admins...")
+            
+            tryTimes += 1
+            
+            if tryTimes == cfg.maxTryTimes:
+                addtionalMsg = 'Try times used up, Fatal error, exit...'    
+            else:
+                addtionalMsg = 'This is try:{}, it is going to retry...'.format(tryTimes)
+            
+                sh.cd(cfg.globalCwd)
+                sh.rm('-r', cfg.projectname)
+
+            ret = ap_email.sendNotifyMail("Autopile Error", exceptionMsg=e.args[0], 
+                addtionalMsg=repr(e) + '\n' + addtionalMsg)
+            if ret:
+                print("Sending Email to Admins successful.")
+            else:
+                print("Sending Failed. exit")
+                exit(1)
+            
+    if tryTimes > cfg.maxTryTimes:
+        exit(2)
+
+    
+    print("Build successful")
+    print("Sending Email to admins...")
+    ap_email.sendNotifyMail("Autopile Build Successful")
+
+
+
